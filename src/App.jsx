a74fc8f0cde625737ae4258b0a17ae7b971a4147
SubhaChandiran <jerrysubash@gmail.com>
2023-10-17 19:10:59 +0530

    day20-task

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,71 @@
 import './App.css'
 
-const Hello = (props) => {
-  console.log(props)
+// const Hello = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>Hello {props.name}, your age is { props.age }</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <>
+//       <div>
+//         <h1>Greeting</h1>
+//       </div>
+//        <Hello name='Subhash' age='26'/>      
+//     </>
+//   )
+// }
+
+function Card(props) {
   return (
     <div>
-      <p>Hello {props.name}, your age is { props.age }</p>
+      <h3>{props.title}</h3>
+      <h4>{props.price}</h4>
+      <h5>{ props.plan}</h5>
     </div>
   )
 }
 
 const App = () => {
+  const cardsData = [
+    {
+      title: 'FREE',
+      price: '$0/month',
+      plan: [
+        'Single User', '50GB Storage', 'Unlimited Public Projects', 'Community Access'
+      ]
+    },
+    {
+      title: 'PLUS',
+      price: '$9/month',
+      plan: [
+        '5 Users', '50GB Storage', 'Unlimited Public Projects', 'Community Access'
+      ]
+    },
+    {
+      title: 'PRO',
+      price: '$49/month',
+      plan: [
+        'Unlimited Users', '50GB Storage', 'Unlimited Public Projects', 'Community Access'
+      ]
+    }
+  ]
   return (
     <>
       <div>
-        <h1>Greeting</h1>
+        {cardsData.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            price={card.price}
+            plan={card.plan}
+          />
+        ))}
       </div>
-       <Hello name='Subhash' age='26'/>      
     </>
   )
 }
