b1afbceac41aee8ecab6ddc8a5fdb194edcbe27d
SubhaChandiran <jerrysubash@gmail.com>
2023-10-26 19:53:08 +0530

    day21_task_update

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,156 @@
-import './App.css'
-import React from 'react';
+// import './App.css'
+// import React from 'react';
 
-function Card({ title, price, plan, titleClassName, priceClassName, planClassName }) {
+import React, { useState } from 'react';
+import './App.css';
+
+// function NavDisplay({ items }) {
+//   const [isShopMenuOpen, setShopMenuOpen] = useState(false);
+
+//   const toggleShopMenu = () => {
+//     setShopMenuOpen(!isShopMenuOpen);
+//   };
+
+//   return (
+//     <div className="container">
+//       <nav>
+//         <ul>
+//           {items.map((item, index) => {
+//             if (item === 'Shop') {
+//               return (
+//                 <li key={index} onClick={toggleShopMenu}>
+//                   {item}
+//                   {isShopMenuOpen && (
+//                     <ul className="products">
+//                       <li>All Products</li>
+//                       <li>Popular Items</li>
+//                       <li>New Arrivals</li>
+//                     </ul>
+//                   )}
+//                 </li>
+//               );
+//             }
+//             return <li key={index}>{item}</li>;
+//           })}
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const items = ['Start Bootstrap', 'Home', 'About', 'Shop'];
+//   return (
+//     <div>
+//       <NavDisplay items={items}
+//       className = "navlist"
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ==============================================================
+
+function NavDisplay({ items }) {
+  const [isShopMenuOpen, setShopMenuOpen] = useState(false);
+
+  const toggleShopMenu = () => {
+    setShopMenuOpen(!isShopMenuOpen);
+  };
   return (
-    <div className='Card'>
-      <div className={titleClassName}>{title}</div>
-      <div className={priceClassName}>{price}</div>
-      <div className={planClassName}>
-        <ul>
-          {plan.map((item, index) => (
-            <li
-              key={index} className={item.disabled ? 'disabled' : ''}>
-              <i className="fa fa-solid fa-check"></i>
-              {item.disabled ? <span className='disable-points'>{item.text}</span> : item.text}
-            </li>
-          ))}
-          <button className='button'>BUTTON</button>
-        </ul>
+    <>
+      <div className='container'>
+        <nav>
+          <ul>
+            {items.map((item, index) => {
+              if (item === 'Shop') {
+                return (
+                  <li key={index} onClick={toggleShopMenu}>
+                    <a href="#">{item.name}</a>
+                    {isShopMenuOpen && (
+                      <ul className='products'>
+                        {item.submenu.map((subitem, subindex) => (
+                          <li key={subindex}>
+                            <a href="#">{subitem.name}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                );
+              }
+              return (
+                <li key={index}><a href="#">{item.name}</a></li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
-    </div>
+    </>
   );
 }
 
-
-const cardsData = [
-  {
-    title: 'FREE',
-    price: '$0/month',
-    plan: [
-      { text: 'Single User', disabled: false },
-      { text: '50GB Storage', disabled: false },
-      { text: 'Unlimited Public Projects', disabled: false },
-      { text: 'Community Access', disabled: false },
-      { text: 'Unlimited Private Projects', disabled: true },
-      { text: 'Dedicated Phone Support', disabled: true },
-      { text: 'Free Subdomain', disabled: true },
-      { text: 'Monthly Status Reports', disabled: true },
-    ]
-  },
-  {
-    title: 'PLUS',
-    price: '$9/month',
-    plan: [
-      { text: '5 Users', disabled: false },
-      { text: '50GB Storage', disabled: false },
-      { text: 'Unlimited Public Projects', disabled: false },
-      { text: 'Community Access', disabled: false },
-      { text: 'Unlimited Private Projects', disabled: false },
-      { text: 'Dedicated Phone Support', disabled: false },
-      { text: 'Free Subdomain', disabled: false },
-      { text: 'Monthly Status Reports', disabled: true },
-    ]
-  },
-  {
-    title: 'PRO',
-    price: '$49/month',
-    plan: [
-      { text: 'Unlimited Users', disabled: false },
-      { text: '50GB Storage', disabled: false },
-      { text: 'Unlimited Public Projects', disabled: false },
-      { text: 'Community Access', disabled: false },
-      { text: 'Unlimited Private Projects', disabled: false },
-      { text: 'Dedicated Phone Support', disabled: false },
-      { text: 'Free Subdomain', disabled: false },
-      { text: 'Monthly Status Reports', disabled: false },
-    ]
-  },
-];
-
-const App = () => {
+function App() {
+  const items = [
+    { name: 'Start Bootstrap', tag: 'tag-1' },
+    { name: 'Home', tag: 'tag-2' },
+    { name: 'About', tag: 'tag-3' },
+    {
+      name: 'Shop',
+      tag: 'tag-4',
+      submenu: [
+        { name: 'All Products', tag: 'tag-5' },
+        { name: 'Popular Items', tag: 'tag-6' },
+        { name: 'New Arrivals', tag: 'tag-7' },
+      ],
+    },
+  ];
   return (
     <>
-      <h1 className='heading'>React Price Card</h1>
-      <p className='firstPara'>
-        Reproduce this user interface using a React.js application with JSX.
-        Write the code <br /> in a dynamic manner.
-      </p>
-      <div className='container'>
-        <ul>
-          <li>
-            {cardsData.map((card, index) => (
-              <div key={index} className=''>
-                <Card
-                  key={index}
-                  title={card.title}
-                  price={card.price}
-                  plan={card.plan}
-                  titleClassName="CardHeading"
-                  priceClassName="Cardprice"
-                  planClassName="CardList"
-                  buttonDisabled={index < 2}
-                />
-              </div>
-            ))}
-          </li>
-        </ul>
+      <div>
+        <NavDisplay items={items} />
+        <form class="d-flex">
+            <button class="btn btn-outline-dark" type="submit">
+              <i class="bi-cart-fill me-1"></i>
+              Cart
+              <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+            </button>
+          </form>
       </div>
     </>
   );
 };
 
-export default App;
+export default App
+// ============================ Original code ==================================
+
+// function NavDisplay({ items }) {
+//   return (
+//     <>
+//       <div className='container'>
+//         <nav>
+//           <ul>
+//           {items.map((item, index) => (
+//           <li key={index}>{ item }</li>
+//           ))}
+//           </ul>
+//         </nav>
+//       </div>
+//     </>
+//   );
+// }
+
+// function App() {
+//   const items = ['Start Bootstrap', 'Home', 'About', 'Shop'];
+//   return (
+//     <>
+//       <div>
+//         <NavDisplay items={items} />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App
