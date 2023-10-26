c68f40d706fd4b21ac130d8bdb8dce308d97871c
SubhaChandiran <jerrysubash@gmail.com>
2023-10-26 23:31:52 +0530

    adding navbar-header-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,11 @@
-// import './App.css'
-// import React from 'react';
-
 import React, { useState } from 'react';
 import './App.css';
+import Header from './header';
+import Section from './Section';
+import Footer from './footer';
 
-// function NavDisplay({ items }) {
-//   const [isShopMenuOpen, setShopMenuOpen] = useState(false);
 
-//   const toggleShopMenu = () => {
-//     setShopMenuOpen(!isShopMenuOpen);
-//   };
 
-//   return (
-//     <div className="container">
-//       <nav>
-//         <ul>
-//           {items.map((item, index) => {
-//             if (item === 'Shop') {
-//               return (
-//                 <li key={index} onClick={toggleShopMenu}>
-//                   {item}
-//                   {isShopMenuOpen && (
-//                     <ul className="products">
-//                       <li>All Products</li>
-//                       <li>Popular Items</li>
-//                       <li>New Arrivals</li>
-//                     </ul>
-//                   )}
-//                 </li>
-//               );
-//             }
-//             return <li key={index}>{item}</li>;
-//           })}
-//         </ul>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// function App() {
-//   const items = ['Start Bootstrap', 'Home', 'About', 'Shop'];
-//   return (
-//     <div>
-//       <NavDisplay items={items}
-//       className = "navlist"
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// ==============================================================
 
 function NavDisplay({ items }) {
   const [isShopMenuOpen, setShopMenuOpen] = useState(false);
@@ -61,33 +15,54 @@ function NavDisplay({ items }) {
   };
   return (
     <>
-      <div className='container'>
-        <nav>
-          <ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container px-4 px-lg-5">
+        <a class="navbar-brand" href="#!">Start Bootstrap</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
             {items.map((item, index) => {
-              if (item === 'Shop') {
-                return (
-                  <li key={index} onClick={toggleShopMenu}>
-                    <a href="#">{item.name}</a>
-                    {isShopMenuOpen && (
-                      <ul className='products'>
-                        {item.submenu.map((subitem, subindex) => (
-                          <li key={subindex}>
-                            <a href="#">{subitem.name}</a>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                );
-              }
-              return (
-                <li key={index}><a href="#">{item.name}</a></li>
-              );
-            })}
+               if (item === 'Shop') {
+                 return (
+                   <li key={index} onClick={toggleShopMenu}>
+                     <a href="#">{item.name}</a>
+                     {isShopMenuOpen && (
+                       <ul className='products'>
+                         {item.submenu.map((subitem, subindex) => (
+                           <li key={subindex}>
+                             <a href="#">{subitem.name}</a>
+                           </li>
+                         ))}
+                       </ul>
+                     )}
+                   </li>
+                 );
+               }
+               return (
+                 <li key={index}><a href="#">{item.name}</a></li>
+               );
+             })}
           </ul>
-        </nav>
+          <form class="d-flex">
+            <button class="btn btn-outline-dark" type="submit">
+              <i class="bi-cart-fill me-1"></i>
+              Cart
+              <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+            </button>
+          </form>
+        </div>
       </div>
+    </nav>
     </>
   );
 }
@@ -111,43 +86,97 @@ function App() {
     <>
       <div>
         <NavDisplay items={items} />
-        <form class="d-flex">
-            <button class="btn btn-outline-dark" type="submit">
-              <i class="bi-cart-fill me-1"></i>
-              Cart
-              <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-            </button>
-          </form>
+        <Header />
+        <Section />
+      </div>
+      <div>
+        <Footer />
       </div>
     </>
   );
 };
 
 export default App
-// ============================ Original code ==================================
+
+
+// import React, { useState } from 'react';
+// import './App.css';
+
 
 // function NavDisplay({ items }) {
+//   const [isShopMenuOpen, setShopMenuOpen] = useState(false);
+
+//   const toggleShopMenu = () => {
+//     setShopMenuOpen(!isShopMenuOpen);
+//   };
 //   return (
 //     <>
-//       <div className='container'>
+//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//         <div className='container px-4 px-lg-5'>
 //         <nav>
 //           <ul>
-//           {items.map((item, index) => (
-//           <li key={index}>{ item }</li>
-//           ))}
+//             {items.map((item, index) => {
+//               if (item === 'Shop') {
+//                 return (
+//                   <li key={index} onClick={toggleShopMenu}>
+//                     <a href="#">{item.name}</a>
+//                     {isShopMenuOpen && (
+//                       <ul className='products'>
+//                         {item.submenu.map((subitem, subindex) => (
+//                           <li key={subindex}>
+//                             <a href="#">{subitem.name}</a>
+//                           </li>
+//                         ))}
+//                       </ul>
+//                     )}
+//                   </li>
+//                 );
+//               }
+//               return (
+//                 <li key={index}><a href="#">{item.name}</a></li>
+//               );
+//             })}
 //           </ul>
 //         </nav>
-//       </div>
+//         </div>
+//         <form class="d-flex">
+//             <button class="btn btn-outline-dark" type="submit">
+//               <i class="bi-cart-fill me-1"></i>
+//               Cart
+//               <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+//             </button>
+//           </form>
+//       </nav>
 //     </>
 //   );
 // }
 
 // function App() {
-//   const items = ['Start Bootstrap', 'Home', 'About', 'Shop'];
+//   const items = [
+//     { name: 'Start Bootstrap', tag: 'tag-1' },
+//     { name: 'Home', tag: 'tag-2' },
+//     { name: 'About', tag: 'tag-3' },
+//     {
+//       name: 'Shop',
+//       tag: 'tag-4',
+//       submenu: [
+//         { name: 'All Products', tag: 'tag-5' },
+//         { name: 'Popular Items', tag: 'tag-6' },
+//         { name: 'New Arrivals', tag: 'tag-7' },
+//       ],
+//     },
+//   ];
 //   return (
 //     <>
 //       <div>
 //         <NavDisplay items={items} />
+//         <form class="d-flex">
+//             <button class="btn btn-outline-dark" type="submit">
+//               <i class="bi-cart-fill me-1"></i>
+//               Cart
+//               <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+//             </button>
+//           </form>
 //       </div>
 //     </>
 //   );
