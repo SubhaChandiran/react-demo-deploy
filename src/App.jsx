43481b12585ce704f8f2cd9c6ba2fedd789d601b
SubhaChandiran <jerrysubash@gmail.com>
2023-11-22 22:47:34 +0530

    day22_task

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Dashboard from "./components/Dashboard";
+import SidePanel from "./components/SidePanel";
+import TopNavBar from "./components/TopNavBar";
+import Footer from "./components/Footer";
+import LogOutModal from "./components/LogOutModal";
 
 
-
-function App() {
-  
+function App({ income }) {
+  const [element, setElement] = useState( <Dashboard income={income} />);
+  const [keyWord, setKeyWord] = useState("Dahsboard");
+  const [homePage, setHomePage] = useState(null);
   return (
-    <>
-      <div>
-        <Username name={ 'Subhash' }/>
-      </div>  
-    </>
-  )
+    <div id="page-top">
+     
+      {homePage ? (
+        homePage
+      ) : (
+        <div id="wrapper">
+          <SidePanel
+            income={income}
+            element={element}
+            setElement={setElement}
+            keyWord={keyWord}
+            setKeyWord={setKeyWord}
+            homePage = {homePage}
+            setHomePage = {setHomePage}
+          />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <TopNavBar />
+              <div className="container-fluid">{element}</div>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      )}
+      <LogOutModal homePage={homePage} setHomePage={setHomePage} />
+    </div>
+  );
 }
 
-export default App
+export default App;
